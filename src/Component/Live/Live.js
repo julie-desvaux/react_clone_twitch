76f9323c6ce7a1c7a0de6b42c1417999ba8e4d46
julie-desvaux <desvaux.julie@gmail.com>
2020-11-20 16:56:07 +0100

    [Feature] add streamer's details in page live

--- src/Component/Live/Live.js
+++ src/Component/Live/Live.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import ReactTwitchEmbedVideo from 'react-twitch-embed-video';
 import iconViewer from './viewer.svg';
-import iconPartner from './partner.svg';
+// import iconPartner from './partner.svg';
 import api from '../../api';
 
 export default function Live() {
@@ -11,6 +11,8 @@ export default function Live() {
     const [infoStream, setInfoStream] = useState([]);
     const [infoGame, setInfoGame] = useState([]);
     const [infoUser, setInfoUser] = useState([]);
+    const [tagsUser, setTagsUser] = useState([]);
+    const [followers, setFollowers] = useState();
 
     useEffect(() => {
         const fetchData = async () => {
@@ -23,14 +25,21 @@ export default function Live() {
             let gameName = resultNameGame.data.data.map(game => {
                 return game.name
             })
+            console.log("result", result);
 
             const resultUser = await api.get(`https://api.twitch.tv/helix/users?id=${result.data.data[0].user_id}`);
-            console.log("resultUser : ", resultUser);
+
+            console.log("resultUser", resultUser);
+
+            const resultTagsUser = await api.get(`https://api.twitch.tv/helix/streams/tags?broadcaster_id=${result.data.data[0].user_id}`);
+
+            const resultFollowers = await api.get(`https://api.twitch.tv/helix/users/follows?to_id=${result.data.data[0].user_id}`)
 
             setInfoStream(result.data.data[0]);
             setInfoGame(gameName);
             setInfoUser(resultUser.data.data[0]);
-
+            setTagsUser(resultTagsUser.data.data);
+            setFollowers(resultFollowers.data.total);
         }
 
         fetchData()
@@ -61,13 +70,37 @@ export default function Live() {
                         </div>
                         <div className="titleStream">{infoStream.title}</div>
                         <div className="nameGame">{infoGame}</div>
-                        <div className="infoGame">{infoStream.language}</div>
+                        <div className="infosGame">
+                            {tagsUser ? tagsUser.map((tagUser, index) => {
+                                    return(
+                                        <div className="infoGame" key={index}><p>{tagUser.localization_names["fr-fr"]}</p></div>
+                                    )
+                                })
+                                : null
+                            }
+                        </div>
+                        
                     </div>
                     <div className="divViewer">
                         <div className="viewer"> <img src={iconViewer} alt="Icone nombre de viewers"/> {infoStream.viewer_count}</div>
                     </div>
+                </div>                
+            </div>
+            <div className="containerInfosUser">
+                <div className="pictureProfilFollowers">
+                    <img 
+                        src={infoUser.profile_image_url} 
+                        alt={`Photo de profil de ${infoUser.user_name}`} 
+                        className="profilRounded"
+                    />
+                    <div className="followers">
+                        {followers} followers
+                    </div>
                 </div>
-                
+                <div className="user-description">
+                    <div className="username">About {infoUser.display_name}</div>
+                    <div className="description">{infoUser.description}</div>
+                </div>
             </div>
         </div>
     )
